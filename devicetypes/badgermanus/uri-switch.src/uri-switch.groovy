11a06fedc099d7ffff4da18c482b855a5576febb
Jonathan Wilson <badgermanus@gmail.com>
2020-12-22 12:58:12 -0800

    Push changes from SmartThings

---
+++ devicetypes/badgermanus/uri-switch.src/uri-switch.groovy
@@ -0,0 +1,78 @@
+/*
+* Author: tguerena and surge919
+* Modified by J Wilson
+*
+* Device Handler
+*/
+
+
+preferences {
+	input "external_on_uri", "text", title: "External On URI", required: false
+	input "external_off_uri", "text", title: "External Off URI", required: false
+}
+
+
+
+
+metadata {
+	definition (name: "URI Switch", namespace: "badgermanus", author: "Troy Guerena") {
+		capability "Actuator"
+			capability "Switch"
+			capability "Sensor"
+	}
+
+	// simulator metadata
+	simulator {
+	}
+
+	// UI tile definitions
+	tiles {
+		standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
+				state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
+		}
+		standardTile("offButton", "device.button", width: 1, height: 1, canChangeIcon: true) {
+			state "default", label: 'Force Off', action: "switch.off", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+		}
+		standardTile("onButton", "device.switch", width: 1, height: 1, canChangeIcon: true) {
+			state "default", label: 'Force On', action: "switch.on", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+		}
+		main "button"
+			details (["button","onButton","offButton"])
+	}
+}
+
+def parse(String description) {
+	log.debug(description)
+}
+
+def on() {
+	if (external_on_uri){
+		// sendEvent(name: "switch", value: "on")
+		// log.debug "Executing ON"
+
+		def cmd = "${settings.external_on_uri}";
+
+		log.debug "Sending request cmd[${cmd}]"
+
+		httpGet(cmd) {resp ->
+			if (resp.data) {
+				log.info "${resp.data}"
+			} 
+		}
+        log.info "Finished on request"
+	}
+}
+
+def off() {
+	if (external_off_uri){
+		def cmd = "${settings.external_off_uri}";
+		log.debug "Sending request cmd[${cmd}]"
+		httpGet(cmd) {resp ->
+			if (resp.data) {
+				log.info "${resp.data}"
+			} 
+		}
+        log.info "Finished off request"
+	}
+}
